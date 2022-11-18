afd41b771e0f4de928b62e25f2c09c9f2a47f53d
Raphael Manke <raphael.manke@codecentric.de>
2022-11-18 10:00:32 +0100

    chore: favor named imports of wildcard
    I prefer to use named imports instead of
    wildcard imports like '* as cdk'

--- lib/cdk-lambda-node-18-stack.ts
+++ lib/cdk-lambda-node-18-stack.ts
@@ -1,16 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export class CdkLambdaNode18Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkLambdaNode18Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaNode18Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
